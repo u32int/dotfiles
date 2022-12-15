a7d60493c9837b3d7f932e81f876cc0e1135fb78
vsh <88970656+u32int@users.noreply.github.com>
2022-12-15 20:18:32 +0100

    remove old scripts

--- .scripts/dwm-apps.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-xinput --set-prop "Logitech USB Receiver" "Coordinate Transformation Matrix" 1.4 0 0 0 1.4 0 0 0 1 &
-lxsession &
-xset r rate 300 50 &
-sh ~/.fehbg &
-picom &
-
-# gentoo-pipewire-launcher &
